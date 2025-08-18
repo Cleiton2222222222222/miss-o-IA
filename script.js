24685b6c356679ef2d400e4eaee5161188f467bc
cleiton <cleiton.agnol@escola.pr.gov.br>
2025-08-18 08:20:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ const caixaPerguntas = documento.querySelector(".caixa-perguntas")
 const caixaAlternativas = documento.querySelector(".caixa-alternativas")
 const caixaResultados = documento.querySelector(".caixa-resultados")
 const textoResultado = documento.querySelector(".texto-resultados")
+
+const perguntas = [
+{
+enunciado:"pergunta 1",
+alternativas:["alternativa 1","alternativa 2"]
+}    
+{    
+enunciado:"pergunta 2"
+alternativas:["alternativa 1", "alternativa 2"]
+}
+{    
+    enunciado:"pergunta 3"
+    alternativas:["alternativa 1", "alternativa 2"]
+    }
+    {    
+        enunciado:"pergunta 4"
+        alternativas:["alternativa 1", "alternativa 2"]
+    }
+    {    
+       enunciado:"pergunta 5"
+       alternativas:["alternativa 1", "alternativa 2"]
+   }
+               
+
+]
