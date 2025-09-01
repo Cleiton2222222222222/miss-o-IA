bc12f2d6571a50cb614e0760435c5163aedf60a4
cleiton <cleiton.agnol@escola.pr.gov.br>
2025-09-01 08:29:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,24 +7,72 @@ const textoResultado = documento.querySelector(".texto-resultados")
 const perguntas = [
 {
 enunciado:"pergunta 1",
-alternativas:["alternativa 1","alternativa 2"]
-}    
+alternativas:[
+    {
+        texto: "alternativa 1",
+        afirmacao: "afirmacao"
+    },
+     {
+        texto: "alternativa 2",
+        afirmacao: "afirmacao"
+     }
+    
+    ]
+},   
 {    
-enunciado:"pergunta 2"
-alternativas:["alternativa 1", "alternativa 2"]
-}
+enunciado: "pergunta 2",
+alternativas: [
+    {
+        texto: "alternativa 1",
+        afirmacao: "afirmacao"
+    },
+     {
+        texto: "alternativa 2",
+        afirmacao: "afirmacao"
+     }
+    ]
+},
 {    
-    enunciado:"pergunta 3"
-    alternativas:["alternativa 1", "alternativa 2"]
-    }
-    {    
-        enunciado:"pergunta 4"
-        alternativas:["alternativa 1", "alternativa 2"]
-    }
-    {    
-       enunciado:"pergunta 5"
-       alternativas:["alternativa 1", "alternativa 2"]
-   }
-               
+    enunciado:"pergunta 3",
+    alternativas:[{
+        texto: "alternativa 1",
+        afirmacao: "afirmacao"
+    },
+     {
+        texto: "alternativa 2",
+        afirmacao: "afirmacao"
+     }]
+},
+{
+    enunciado:"pergunta 4",
+    alternativas:[{
+        texto: "alternativa 1",
+        afirmacao: "afirmacao"
+    },
+     {
+        texto: "alternativa 2",
+        afirmacao: "afirmacao"
+     }]
+},
+{    
+   enunciado:"pergunta 5",
+   alternativas:[{
+    texto: "alternativa 1",
+    afirmacao: "afirmacao"
+},
+ {
+    texto: "alternativa 2",
+    afirmacao: "afirmacao"
+ }]
+},               
 
-]
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostrarperguntas(){
+ perguntaAtual = pergunta[atual];  
+ caixaPerguntas. textcontent = perguntaAtual. enunciado;
+}
+mostrarperguntas();
