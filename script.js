7b87843c1ba0805a3cc2739765e58ce6bfd28a32
cleiton <cleiton.agnol@escola.pr.gov.br>
2025-09-29 08:21:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,73 +6,72 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Você é um jovem do interior que encontra uma antiga câmera no sótão da casa do seu avô. Dentro dela, há rolos de filmes e um bilhete:Quem entender Mazzaropi, entende o Brasil.Você decide embarcar numa viagem para descobrir o legado de Amácio Mazzaropi, o caipira mais famoso do cinema nacional.A cada decisão, você molda o final dessa jornada;Qual é a principal característica dos personagens interpretados por Mazzaropi?",
+
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Humor caipira e crítica social disfarçada.",
+                afirmacao: "No início ficou em duvida sobre o mundo do mazzaropi. "
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Violência e ação no campo.",
+                afirmacao: "você não conhece bem o mundo do mazzaropi."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Em (O Corintiano) (1966), Mazzaropi interpreta um torcedor fanático. O que esse filme retrata além do futebol?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "A relação entre fanatismo esportivo e o cotidiano da classe média.",
+                afirmacao: "começando a entender melhor o mundo do mazzaropi."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "A ascensão de clubes milionários no futebol europeu. ",
+                afirmacao: "não está entendendo o mundo do mazzaropi."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Mazzaropi produzia seus próprios filmes. Qual foi o estúdio criado por ele?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "PAM Filmes.",
+                afirmacao: "agora sim está entendendo mais ainda."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Estúdios Globo Rural.",
+                afirmacao: "é da globo né então pense bem."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Por que os filmes de Mazzaropi foram considerados à margem do cinema “oficial” brasileiro por muitos críticos da época?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Por seu tom popular, com foco em humor simples e linguagem acessível.",
+                afirmacao: "se tu respondeu é porque sabe."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: " Porque ele filmava apenas em Hollywood e ignorava a cultura brasileira.",
+                afirmacao: "claramente não era essa."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "O legado de Mazzaropi ainda é lembrado hoje em... ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Museus, festivais e homenagens no interior paulista.",
+                afirmacao: "só os brabo respondem certo."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Videogames e séries de ficção científica.",
+                afirmacao: "tu é burro cara. "
             }
         ]
     },
 ];
-
-
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
@@ -105,7 +104,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "após tudo isso você percebeu que tu manja do mundo do mazzaropi";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
